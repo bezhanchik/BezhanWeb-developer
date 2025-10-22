ad12577ac9c3dea9fb59a6b897606c4c796fb1ac
Bezhan <nepyxlox@gmail.com>
2025-10-22 19:37:42 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+new WOW().init();
+
+var MySwiper = new Swiper('.swiper', {
+    pagination: {
+        el: '.project-pagination',
+        bulletClass: 'project-bullet',
+        bulletActiveClass: 'project-bullet-active',
+        clickable: true
+    },
+});
